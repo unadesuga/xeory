830e35c7840d32ba4dea2d8ac7f418ab2e5340e3
unadesuga <unauna@YOSUKEnoMacBook-puro.local>
2018-09-10 12:22:10 +0700

    First message

---
+++ script.js
@@ -0,0 +1,25 @@
+$(function(){
+  $('#icon-hamburger').click(function(){
+    $('.menu-container').slideDown();
+  });
+
+  $('#close').click(function(){
+    $('.menu-container').slideUp();
+  });
+
+  var pagetop = $('.pagetop');
+     $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+             pagetop.fadeIn();
+        } else {
+             pagetop.fadeOut();
+             }
+        });
+        pagetop.click(function () {
+            $('body, html').animate({ scrollTop: 0 }, 500);
+               return false;
+    });
+
+
+
+});
